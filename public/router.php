245c25a3577fc50d7f23b3550893d174af77f74e
chengyao <bigyao@139.com>
2020-12-20 11:52:42 +0800

    内置服务器优化

---
+++ public/router.php
@@ -0,0 +1,10 @@
+<?php
+
+if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
+    return false;
+} else {
+    define('ROOT', getcwd() . '/');
+    require ROOT . 'vendor/autoload.php';
+    //调用框架初始化方法
+    yao\App::run();
+}
--- route/route.php
+++ route/route.php
@@ -7,3 +7,5 @@
 use yao\facade\Route;
 
 Route::get('/', [\app\index\controller\Index::class, 'index']);
+
+Route::get('/index.html', [\app\index\controller\Index::class, 'index']);
