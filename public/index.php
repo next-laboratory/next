0853ac7aa0a630ea14193237a66b1270d2f8cc02
chengyao <bigyao@139.com>
2020-12-20 12:41:24 +0800

    关闭错误报告

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
-//框架开发阶段开启，线上一定要删掉或注释掉
-ini_set('display_errors', 'on');
-error_reporting(E_ALL);
-
 define('ROOT', dirname(getcwd()) . '/');
 require ROOT . 'vendor/autoload.php';
 //调用框架初始化方法
