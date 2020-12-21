6009c0c797c86bbab1a170ec300758171079fa54
chengyao <64066545+topyao@users.noreply.github.com>
2020-12-21 08:50:36 +0800

    Update router.php

--- public/router.php
+++ public/router.php
@@ -6,5 +6,5 @@ if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
     define('ROOT', getcwd() . '/');
     require ROOT . 'vendor/autoload.php';
     //调用框架初始化方法
-    yao\App::run();
+    Yao\App::run();
 }
