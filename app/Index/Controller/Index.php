498c726cd7478a672668f58db33dfdc572afeccd
chengyao <64066545+topyao@users.noreply.github.com>
2020-12-21 08:51:31 +0800

    Update Index.php

--- app/Index/Controller/Index.php
+++ app/Index/Controller/Index.php
@@ -2,23 +2,10 @@
 
 namespace App\Index\Controller;
 
-use yao\facade\Request;
-
 class Index
 {
     public function index()
     {
-        if (Request::isMethod('get')) {
-            return view('index@index');
-        }
-        dump(Request::get());
-    }
-
-    public function list()
-    {
-        echo '111';
-//        $name = new \yao\http\Request();
-//        $ref = new ReflectionObject($name);
-//        dump($ref);
+        abort('666');
     }
 }
