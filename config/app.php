c1a4a510a47d1d6fee2df0fceebb16bf34df3f8c
chengyao <bigyao@139.com>
2021-06-14 22:48:10 +0800

    dev -> rename exception handler class name

--- config/app.php
+++ config/app.php
@@ -2,28 +2,28 @@
 
 return [
     //是否开启调试
-    'debug'            => env('app.debug', true),
+    'debug'             => env('app.debug', true),
     //是否记录日志
-    'log'              => env('app.log', true),
+    'log'               => env('app.log', true),
     //自动开启session
-    'auto_start'       => env('app.auto_start', false),
+    'auto_start'        => env('app.auto_start', false),
     //默认时区
-    'default_timezone' => env('app.default_timezone', 'PRC'),
+    'default_timezone'  => env('app.default_timezone', 'PRC'),
     //参数过滤
-    'filter'           => ['trim', 'htmlspecialchars'],
+    'filter'            => ['trim', 'htmlspecialchars'],
     //默认语言
-    'language'         => 'zh',
-    //异常句柄， null使用框架的异常界面
-    'exception_handle' => null,
+    'language'          => 'zh',
+    //异常处理类
+    'exception_handler' => Handler::class,
     //类别名
-    'alias'            => [],
+    'alias'             => [],
     //全局中间件
-    'middleware'       => [
+    'middleware'        => [
 //        \App\Http\Middleware\BasicAuth::class
 //        \Max\Http\Middleware\GlobalCross::class,
     ],
     //服务提供者
-    'provider'         => [
+    'provider'          => [
         'http' => [
             \Max\DatabaseService::class,
             \Max\CacheService::class,
@@ -40,5 +40,5 @@ return [
         ],
     ],
     //响应Header中的Powered-By
-    'powered_by'       => 'MaxPHP'
+    'powered_by'        => 'MaxPHP'
 ];
