253eead729af72b7617880ab3e4b91a4c849a3b4
chengyao <bigyao@139.com>
2021-08-11 20:43:32 +0800

    入口优化

--- public/index.php
+++ public/index.php
@@ -5,10 +5,8 @@ namespace Max;
 require __DIR__ . '/../vendor/autoload.php';
 
 (new App())->start(function (App $app) {
-
     $http     = $app->http;
     $response = $http->response();
 
     $http->end($response);
-
 });
