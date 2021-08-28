813d1bab4cf7457595540138e052e2b12a262aa0
chengyao <bigyao@139.com>
2021-08-28 16:02:09 +0800

    update

--- app/Http/Controllers/Index.php
+++ app/Http/Controllers/Index.php
@@ -4,16 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Controller;
 use App\Http\Requests\UserRequest;
-use Max\Facade\DB;
 
 class Index extends Controller
 {
 
     public function index()
     {
-        DB::name('users')->select()->each(function ($item) {
-            dump($item);
-        });
         return view('index');
     }
 
