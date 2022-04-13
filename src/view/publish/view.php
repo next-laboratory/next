544771046de4c87c4bca79bcd651c15d91bc17f9
chengyao <bigyao@139.com>
2022-04-13 09:52:49 +0800

    修改编译路径

--- src/view/publish/view.php
+++ src/view/publish/view.php
@@ -18,7 +18,7 @@ return [
                 // 模板目录
                 'path'       => __DIR__ . '/../views/',
                 // 编译和缓存目录
-                'compileDir' => __DIR__ . '/../runtime/cache/views/compile/',
+                'compileDir' => __DIR__ . '/../runtime/cache/views/',
                 // 模板缓存
                 'cache'      => false,
                 // 模板后缀
