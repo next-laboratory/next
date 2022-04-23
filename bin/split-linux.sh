4cd69508ba12eea3da258d43fba24df2ee6c20d7
chengyao <bigyao@139.com>
2022-04-23 22:16:58 +0800

    添加分离脚本

--- bin/split-linux.sh
+++ bin/split-linux.sh
@@ -25,7 +25,7 @@ if [[ $# -eq 0 ]]; then
 fi
 
 for REPO in $REPOS ; do
-    remote $REPO git@github.com:topyao/$REPO.git
+    remote $REPO git@github.com:topyao/max-$REPO.git
 
     split "src/$REPO" $REPO
 done
