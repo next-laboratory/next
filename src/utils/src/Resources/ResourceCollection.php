760fc1792de39ebfc818ba235c6894b264b8350a
chengyao <bigyao@139.com>
2022-04-28 13:24:30 +0800

    [pref] 支持PHP7.4

--- src/utils/src/Resources/ResourceCollection.php
+++ src/utils/src/Resources/ResourceCollection.php
@@ -1,23 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * This file is part of the Max package.
+ *
+ * (c) Cheng Yao <987861463@qq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Max\Utils\Resources;
 
 use JsonSerializable;
-use ReturnTypeWillChange;
 
 class ResourceCollection implements JsonSerializable
 {
     /**
+     * @var
+     */
+    protected $collection;
+
+    /**
      * @param $collection
      */
-    public function __construct(public $collection)
+    public function __construct($collection)
     {
+        $this->collection = $collection;
     }
 
     /**
      * @return string
      */
-    #[ReturnTypeWillChange]
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return $this->collection;
