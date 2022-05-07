072c4a9019cc0e3d0f09adfbbcf26ea8f9020d68
chengyao <bigyao@139.com>
2022-05-07 08:56:54 +0800

    [pref] 添加配置提供者

---
+++ src/framework/src/ConfigProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Max package.
+ *
+ * (c) Cheng Yao <987861463@qq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Max\Framework;
+
+class ConfigProvider
+{
+    /**
+     * @return string[][]
+     */
+    public function __invoke(): array
+    {
+        return [
+            'commands' => [
+                'Max\Framework\Console\Commands\Make',
+                'Max\Framework\Console\Commands\Queue',
+                'Max\Framework\Console\Commands\RouteList',
+                'Max\Framework\Console\Commands\Server',
+                'Max\Framework\Console\Commands\VendorPublish',
+            ]
+        ];
+    }
+}
