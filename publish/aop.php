f92466fa592090f53c42e589b4289aa3c96d93b9
chengyao <bigyao@139.com>
2022-05-14 11:46:55 +0800

    [feat] 添加许可

---
+++ publish/aop.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Max package.
+ *
+ * (c) Cheng Yao <987861463@qq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'cache'      => false,
+    'paths'      => [
+        __DIR__ . '/app',
+    ],
+    'collectors' => [],
+    'runtimeDir' => __DIR__ . '/runtime',
+];
