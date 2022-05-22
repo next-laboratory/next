4f39e1fa2381df765437cd6a0fad5c20bf9994ad
ChengYao <bigyao@139.com>
2022-05-22 11:03:59 +0800

    [pref] workerman添加configProvider

---
+++ src/workerman/src/ConfigProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Max package.
+ *
+ * (c) Cheng Yao <987861463@qq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Max\Workerman;
+
+class ConfigProvider
+{
+    /**
+     * @return array
+     */
+    public function __invoke(): array
+    {
+        return [];
+    }
+
+    /**
+     * publish
+     */
+    public function publish()
+    {
+        $to = dirname(__DIR__, 4) . '/config/workerman.php';
+        file_exists($to) || copy(__DIR__ . '/../publish/workerman.php', $to);
+    }
+}
