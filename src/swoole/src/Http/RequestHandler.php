86549856f00b245b62cb02d6cdbf243e5581e77c
ChengYao <bigyao@139.com>
2022-06-02 13:21:15 +0800

    [pref] 控制器方法必须返回ResponseInterface对象

--- src/swoole/src/Http/RequestHandler.php
+++ src/swoole/src/Http/RequestHandler.php
@@ -13,17 +13,13 @@ declare(strict_types=1);
 
 namespace Max\Swoole\Http;
 
+use Closure;
 use Max\Di\Context;
-use Max\Di\Exceptions\NotFoundException;
 use Max\Http\Exceptions\InvalidRequestHandlerException;
-use Max\Http\Exceptions\InvalidResponseBodyException;
-use Max\Http\Message\Stream\StringStream;
 use Max\Http\Server\RequestHandler as PsrRequestHandler;
 use Max\Routing\Route;
 use Max\Routing\RouteCollector;
 use Max\Routing\Router;
-use Max\Utils\Contracts\Arrayable;
-use Max\Utils\Stringable;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -34,56 +30,36 @@ class RequestHandler implements RequestHandlerInterface
 {
     /**
      * 全局中间件
-     *
-     * @var array
      */
-    protected array $middlewares = [];
-
-    /**
-     * @var Router
-     */
+    protected array  $middlewares = [];
     protected Router $router;
 
-    /**
-     * @param RouteCollector    $routeCollector
-     * @param ResponseInterface $response
-     */
     public function __construct(RouteCollector $routeCollector, protected ResponseInterface $response)
     {
         $this->map($this->router = new Router([], $routeCollector));
     }
 
-    /**
-     * @param Router $router
-     */
     protected function map(Router $router)
     {
     }
 
     /**
-     * @param ServerRequestInterface $request
-     *
-     * @return ResponseInterface
-     * @throws NotFoundException
-     * @throws ReflectionException
-     * @throws InvalidRequestHandlerException
      * @throws ContainerExceptionInterface
+     * @throws InvalidRequestHandlerException
+     * @throws ReflectionException
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         return (new PsrRequestHandler())
             ->setContainer(Context::getContainer())
-            ->setRequestHandler(\Closure::fromCallable([$this, 'handleRequest']))
+            ->setRequestHandler(Closure::fromCallable([$this, 'handleRequest']))
             ->setMiddlewares($this->middlewares)
             ->handle($request);
     }
 
     /**
-     * @param ServerRequestInterface $request
-     *
-     * @return ResponseInterface
      * @throws ContainerExceptionInterface
-     * @throws ReflectionException|InvalidResponseBodyException
+     * @throws ReflectionException
      */
     protected function handleRequest(ServerRequestInterface $request): ResponseInterface
     {
@@ -98,62 +74,11 @@ class RequestHandler implements RequestHandlerInterface
             [$controller, $action] = $action;
             $action = [Context::getContainer()->make($controller), $action];
         }
-
-        return $this->autoResponse(call($action, array_filter($params, fn($value) => !is_null($value))));
+        // 控制器方法必须返回Response实例
+        return call($action, array_filter($params, fn($value) => !is_null($value)));
     }
 
     /**
-     * @param $response
-     *
-     * @return ResponseInterface
-     * @throws InvalidResponseBodyException
-     */
-    protected function autoResponse($response): ResponseInterface
-    {
-        if (!$response instanceof ResponseInterface) {
-            $response = match (true) {
-                $response instanceof Arrayable => $this->jsonResponse($response->toArray()),
-                $response instanceof Stringable => $this->htmlResponse($response->__toString()),
-                is_scalar($response) || is_null($response) => $this->htmlResponse((string)$response),
-                default => $this->jsonResponse($response),
-            };
-        }
-        return $response;
-    }
-
-    /**
-     * @param $data
-     *
-     * @return ResponseInterface
-     * @throws InvalidResponseBodyException
-     */
-    protected function jsonResponse($data): ResponseInterface
-    {
-        try {
-            return $this->response
-                ->withHeader('Content-Type', 'application/json; charset=utf-8')
-                ->withBody(new StringStream(json_encode($data, JSON_UNESCAPED_UNICODE)));
-        } catch (\Throwable $throwable) {
-            throw new InvalidResponseBodyException($throwable->getMessage(), $throwable->getCode(), $throwable);
-        }
-    }
-
-    /**
-     * @param $data
-     *
-     * @return ResponseInterface
-     */
-    protected function htmlResponse($data): ResponseInterface
-    {
-        return $this->response
-            ->withHeader('Content-Type', 'text/html; charset=utf-8')
-            ->withBody(new StringStream($data));
-    }
-
-    /**
-     * @param string $name
-     * @param array  $arguments
-     *
      * @return mixed
      */
     public function __call(string $name, array $arguments)
