3fcfce24fd08abfcb7a8c43d394c7adb9f88e9e5
ChengYao <bigyao@139.com>
2022-06-02 13:19:55 +0800

    [pref] 添加方法参数注解收集

--- src/aop/src/Contracts/CollectorInterface.php
+++ src/aop/src/Contracts/CollectorInterface.php
@@ -20,4 +20,6 @@ interface CollectorInterface
     public static function collectMethod(string $class, string $method, object $attribute): void;
 
     public static function collectProperty(string $class, string $property, object $attribute): void;
+
+    public static function collectorMethodParameter(string $class, string $method, string $parameter, object $attribute);
 }
