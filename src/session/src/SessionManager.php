8f8d612e3e6ae3dfa559563a5879ed74f43f71d4
chengyao <64066545+topyao@users.noreply.github.com>
2022-06-05 12:00:21 +0800

    [pref] 不重新初始化handlers

--- src/session/src/SessionManager.php
+++ src/session/src/SessionManager.php
@@ -12,6 +12,11 @@ class SessionManager
     protected array $config = [];
 
     /**
+     * @var array
+     */
+    protected array $handlers = [];
+
+    /**
      * @param ConfigInterface $config
      */
     public function __construct(ConfigInterface $config)
@@ -26,10 +31,14 @@ class SessionManager
      */
     public function create(?string $name = null): Session
     {
-        $name    ??= $this->config['default'];
-        $config  = $this->config['stores'][$name];
-        $handler = $config['handler'];
-        $options = $config['options'];
-        return new Session(new $handler($options));
+        $name ??= $this->config['default'];
+        if (!isset($this->handlers[$name])) {
+            $config                = $this->config['stores'][$name];
+            $handler               = $config['handler'];
+            $options               = $config['options'];
+            $this->handlers[$name] = new $handler($options);
+        }
+
+        return new Session($this->handlers[$name]);
     }
 }
