e8984c181e2a7c2e86260ac750f7d8d2decd8a54
chengyao <64066545+topyao@users.noreply.github.com>
2022-06-25 08:45:23 +0800

    [doc] 更改header位置

--- src/http-server/src/ResponseEmitter/FPMResponseEmitter.php
+++ src/http-server/src/ResponseEmitter/FPMResponseEmitter.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace Max\Http\Server\ResponseEmitter;
 
 use Max\Http\Message\Cookie;
-use Max\Http\Message\Stream\FileStream;
 use Max\Http\Server\Contracts\ResponseEmitterInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -22,7 +21,7 @@ class FPMResponseEmitter implements ResponseEmitterInterface
 {
     /**
      * @param ResponseInterface $psrResponse
-     * @param null $sender
+     * @param null              $sender
      *
      * @return void
      */
@@ -43,16 +42,6 @@ class FPMResponseEmitter implements ResponseEmitterInterface
             header($name . ': ' . implode(', ', $value));
         }
         $body = $psrResponse->getBody();
-        if ($body instanceof FileStream) {
-            header("Pragma: public");                                            //1 Public指示响应可被任何缓存区缓存。
-            header("Expires: 0");                                                //2 浏览器不会响应缓存
-            header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); //3
-            //            header("Content-Type:application/force-download");                  //4 请求该页面就出现下载保存窗口。
-            //            header("Content-Type:application/octet-stream");                    //5  二进制流，不知道下载文件类型。
-            //            header("Content-Type:application/vnd.ms-excel;");                   //6
-            header("Content-Type: application/download");                        //7 提示用户将当前文件保存到本地。
-            header("Content-Transfer-Encoding: binary");                         //9
-        }
         echo $body?->getContents();
         $body?->close();
     }
