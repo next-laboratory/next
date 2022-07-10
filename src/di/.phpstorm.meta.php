70794fae1998725be8d91800fac9a91ce8373d72
chengyao <64066545+topyao@users.noreply.github.com>
2022-07-10 13:20:53 +0800

    [pref] 添加PHPSTORM_META

--- src/di/.phpstorm.meta.php
+++ src/di/.phpstorm.meta.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace PHPSTORM_META {
-    // Reflect
     override(\Psr\Container\ContainerInterface::get(0), map('@'));
     override(\Psr\Container\ContainerInterface::make(0), map('@'));
+    override(\Max\Di\Container::make(0), map('@'));
+    override(\Max\Di\Container::make(0), map('@'));
     override(\make(0), map('@'));
 }
