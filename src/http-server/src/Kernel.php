a3d0b49631ea461c4de26a9eae557c739160f34a
chengyao <bigyao@139.com>
2022-07-24 10:21:00 +0800

    [pref] 异常处理优化

--- src/http-server/src/Kernel.php
+++ src/http-server/src/Kernel.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace Max\Http\Server;
 
-use BadMethodCallException;
 use Max\Http\Server\Events\OnRequest;
 use Max\Routing\Exceptions\RouteNotFoundException;
 use Max\Routing\RouteCollector;
