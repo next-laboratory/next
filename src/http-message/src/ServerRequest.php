50f5889b93ab3daa3ca97296a9dff0c5a52e413c
chengyao <64066545+topyao@users.noreply.github.com>
2022-08-14 15:19:52 +0800

    Pref: 优化is方法

--- src/http-message/src/ServerRequest.php
+++ src/http-message/src/ServerRequest.php
@@ -73,11 +73,11 @@ class ServerRequest extends Request implements ServerRequestInterface
                 $hasPort = true;
                 $uri     = $uri->withPort($hostHeaderParts[1]);
             }
-        } elseif (isset($server['server_name'])) {
+        } else if (isset($server['server_name'])) {
             $uri = $uri->withHost($server['server_name']);
-        } elseif (isset($server['server_addr'])) {
+        } else if (isset($server['server_addr'])) {
             $uri = $uri->withHost($server['server_addr']);
-        } elseif (isset($header['host'])) {
+        } else if (isset($header['host'])) {
             $hasPort = true;
             if (strpos($header['host'], ':')) {
                 [$host, $port] = explode(':', $header['host'], 2);
@@ -91,7 +91,7 @@ class ServerRequest extends Request implements ServerRequestInterface
             $uri = $uri->withHost($host);
         }
 
-        if (! $hasPort && isset($server['server_port'])) {
+        if (!$hasPort && isset($server['server_port'])) {
             $uri = $uri->withPort($server['server_port']);
         }
 
@@ -105,7 +105,7 @@ class ServerRequest extends Request implements ServerRequestInterface
             }
         }
 
-        if (! $hasQuery && isset($server['query_string'])) {
+        if (!$hasQuery && isset($server['query_string'])) {
             $uri = $uri->withQuery($server['query_string']);
         }
 
@@ -115,7 +115,7 @@ class ServerRequest extends Request implements ServerRequestInterface
         $psrRequest->body          = new StringStream($request->getContent());
         $psrRequest->cookieParams  = new CookieBag($request->cookie ?? []);
         $psrRequest->queryParams   = new ParameterBag($request->get ?? []);
-        $psrRequest->uploadedFiles = new FileBag($request->files    ?? []); // TODO Convert to UploadedFiles.
+        $psrRequest->uploadedFiles = new FileBag($request->files ?? []); // TODO Convert to UploadedFiles.
         $psrRequest->attributes    = new ParameterBag($attributes);
 
         return $psrRequest;
@@ -273,7 +273,7 @@ class ServerRequest extends Request implements ServerRequestInterface
     public function withParsedBody($data)
     {
         $new             = clone $this;
-        $new->parsedBody = $data instanceof ParameterBag ? $data : new ParameterBag((array) $data);
+        $new->parsedBody = $data instanceof ParameterBag ? $data : new ParameterBag((array)$data);
 
         return $new;
     }
@@ -324,10 +324,10 @@ class ServerRequest extends Request implements ServerRequestInterface
     public function getRealIp(): string
     {
         $headers = $this->getHeaders();
-        if (isset($headers['x-forwarded-for'][0]) && ! empty($headers['x-forwarded-for'][0])) {
+        if (isset($headers['x-forwarded-for'][0]) && !empty($headers['x-forwarded-for'][0])) {
             return $headers['x-forwarded-for'][0];
         }
-        if (isset($headers['x-real-ip'][0]) && ! empty($headers['x-real-ip'][0])) {
+        if (isset($headers['x-real-ip'][0]) && !empty($headers['x-real-ip'][0])) {
             return $headers['x-real-ip'][0];
         }
         $serverParams = $this->getServerParams();
@@ -348,7 +348,11 @@ class ServerRequest extends Request implements ServerRequestInterface
      */
     public function is(string $pattern): bool
     {
-        return Str::is($pattern, trim($this->getUri()->getPath(), '/'));
+        if (($path = $this->getUri()->getPath()) !== '/') {
+            $path = trim($path, '/');
+        }
+        $pattern = $pattern === '/' ? $pattern : trim($pattern, '/');
+        return Str::is($pattern, $path);
     }
 
     /**
@@ -374,7 +378,7 @@ class ServerRequest extends Request implements ServerRequestInterface
     {
         $uri = $this->getUri();
         $url = $uri->getPath();
-        if (! empty($query = $uri->getQuery())) {
+        if (!empty($query = $uri->getQuery())) {
             $url .= '?' . $query;
         }
         return $url;
