5c0b5e6cb1bb815007bfefc26fed0d6314a5b6b5
chengyao <bigyao@139.com>
2022-09-12 12:20:36 +0800

    Pref: exception handle

--- src/http-server/src/Middleware/ExceptionHandleMiddleware.php
+++ src/http-server/src/Middleware/ExceptionHandleMiddleware.php
@@ -16,6 +16,7 @@ use Max\Http\Message\Contract\StatusCodeInterface;
 use Max\Http\Message\Exception\HttpException;
 use Max\Http\Message\Response;
 use Max\Http\Server\Contract\Renderable;
+use Max\Utils\Arr;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -25,42 +26,51 @@ use Throwable;
 class ExceptionHandleMiddleware implements MiddlewareInterface
 {
     /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<Throwable>>
+     */
+    protected array $dontReport = [];
+
+    /**
      * @throws Throwable
      */
     final public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         try {
             return $handler->handle($request);
-        } catch (Throwable $throwable) {
-            $this->report($throwable, $request);
+        } catch (Throwable $e) {
+            if (!$this->shouldntReport($e)) {
+                $this->report($e, $request);
+            }
 
-            return $this->render($throwable, $request);
+            return $this->render($e, $request);
         }
     }
 
     /**
      * 报告异常.
      */
-    protected function report(Throwable $throwable, ServerRequestInterface $request): void
+    protected function report(Throwable $e, ServerRequestInterface $request): void
     {
     }
 
     /**
      * 将异常转为ResponseInterface对象
      */
-    protected function render(Throwable $throwable, ServerRequestInterface $request): ResponseInterface
+    protected function render(Throwable $e, ServerRequestInterface $request): ResponseInterface
     {
-        if($throwable instanceof Renderable) {
-            return $throwable->render($request);
+        if ($e instanceof Renderable) {
+            return $e->render($request);
         }
-        $message    = $throwable->getMessage();
-        $statusCode = $this->getStatusCode($throwable);
+        $message    = $e->getMessage();
+        $statusCode = $this->getStatusCode($e);
         if (str_contains($request->getHeaderLine(HeaderInterface::HEADER_ACCEPT), 'application/json')
             || strcasecmp('XMLHttpRequest', $request->getHeaderLine('X-REQUESTED-WITH')) === 0) {
             return new Response($statusCode, [], json_encode([
                 'status'  => false,
                 'code'    => $statusCode,
-                'data'    => $throwable->getTrace(),
+                'data'    => $e->getTrace(),
                 'message' => $message,
             ], JSON_UNESCAPED_UNICODE));
         }
@@ -68,12 +78,28 @@ class ExceptionHandleMiddleware implements MiddlewareInterface
             '<html lang="zh"><head><title>%s</title></head><body><pre style="font-size: 1.5em; white-space: break-spaces"><p><b>%s</b></p><b>Stack Trace</b><br>%s</pre></body></html>',
             $message,
             $message,
-            $throwable->getTraceAsString(),
+            $e->getTraceAsString(),
         ));
     }
 
-    protected function getStatusCode(Throwable $throwable)
+    protected function getStatusCode(Throwable $e)
     {
-        return $throwable instanceof HttpException ? $throwable->getCode() : StatusCodeInterface::STATUS_INTERNAL_SERVER_ERROR;
+        return $e instanceof HttpException ? $e->getCode() : StatusCodeInterface::STATUS_INTERNAL_SERVER_ERROR;
+    }
+
+    /**
+     * 忽略报告的异常
+     */
+    protected function shouldntReport(Throwable $e): bool
+    {
+        return !is_null(Arr::first($this->dontReport, fn($type) => $e instanceof $type));
+    }
+
+    /**
+     * 运行环境是否是cli
+     */
+    protected function runInConsole(): bool
+    {
+        return PHP_SAPI === 'cli';
     }
 }
