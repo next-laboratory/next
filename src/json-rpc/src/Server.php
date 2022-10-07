100e0122f49534d05514c2afa841249b034ac8f1
chengyao <bigyao@139.com>
2022-10-07 14:10:19 +0800

    Add: register方法

--- src/json-rpc/src/Server.php
+++ src/json-rpc/src/Server.php
@@ -12,52 +12,80 @@ declare(strict_types=1);
 namespace Max\JsonRpc;
 
 use BadMethodCallException;
+use InvalidArgumentException;
+use Max\Di\Reflection;
 use Max\Http\Message\Contract\HeaderInterface;
-use Max\Http\Message\Response as HttpResponse;
+use Max\Http\Message\Response as PsrResponse;
 use Max\Http\Message\Stream\StandardStream;
 use Max\JsonRpc\Message\Error;
 use Max\JsonRpc\Message\Request;
 use Max\JsonRpc\Message\Response as RpcResponse;
+use Max\Utils\Arr;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use ReflectionException;
+use ReflectionMethod;
 use Throwable;
 
 class Server
 {
+    protected array $services = [];
+
     public function serveHttp(ServerRequestInterface $request): ResponseInterface
     {
         try {
             $rpcRequest = Request::createFromPsrRequest($request);
-            if (is_null($service = ServiceCollector::getService($rpcRequest->getMethod()))) {
+            if (is_null($service = $this->getService($rpcRequest->getMethod()))) {
                 throw new BadMethodCallException('Method Not found', -32601);
             }
-            $result   = call($service, $rpcRequest->getParams());
-            $response = new HttpResponse();
+            $result      = call($service, $rpcRequest->getParams());
+            $psrResponse = new PsrResponse();
             if ($rpcRequest->hasId()) {
-                $content  = json_encode([
+                $content     = json_encode([
                     'jsonrpc' => $rpcRequest->getJsonrpc(),
-                    'result'  => $result,
                     'id'      => $rpcRequest->getId(),
+                    'result'  => $result,
                 ]);
-                $response = $response->withHeader(HeaderInterface::HEADER_CONTENT_TYPE, 'application/json; charset=utf-8')
-                                     ->withBody(StandardStream::create($content));
+                $psrResponse = $psrResponse->withHeader(HeaderInterface::HEADER_CONTENT_TYPE, 'application/json; charset=utf-8')
+                                           ->withBody(StandardStream::create($content));
             }
 
-            return $response;
+            return $psrResponse;
         } catch (Throwable $e) {
-            $rpcResponse = new RpcResponse(
-                null,
-                isset($rpcRequest) ? $rpcRequest->getId() : null,
-                new Error($e->getCode(), $e->getMessage(), [
-                    'file'  => $e->getFile(),
-                    'line'  => $e->getLine(),
-                    'trace' => $e->getTrace(),
-                ])
-            );
-            return new HttpResponse(
-                headers: [HeaderInterface::HEADER_CONTENT_TYPE => 'application/json; charset=utf-8'],
-                body: StandardStream::create(json_encode($rpcResponse))
-            );
+            $psrResponse = new PsrResponse();
+            if (isset($rpcRequest) && $rpcRequest->hasId()) {
+                $rpcResponse = new RpcResponse(
+                    null,
+                    $rpcRequest->getId(),
+                    new Error($e->getCode(), $e->getMessage(), [
+                        'file'  => $e->getFile(),
+                        'line'  => $e->getLine(),
+                        'trace' => $e->getTrace(),
+                    ])
+                );
+                $psrResponse = $psrResponse->withHeader(HeaderInterface::HEADER_CONTENT_TYPE, 'application/json; charset=utf-8')
+                                           ->withBody(StandardStream::create(json_encode($rpcResponse, JSON_UNESCAPED_UNICODE)));;
+            }
+            return $psrResponse;
         }
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function register(string $name, string $class): void
+    {
+        if (isset($this->services[$name])) {
+            throw new InvalidArgumentException('Service \'' . $name . '\' has been registered');
+        }
+        foreach (Reflection::methods($class, ReflectionMethod::IS_PUBLIC) as $reflectionMethod) {
+            $reflectionMethodName                         = $reflectionMethod->getName();
+            $this->services[$name][$reflectionMethodName] = [$class, $reflectionMethodName];
+        }
+    }
+
+    protected function getService(string $name)
+    {
+        return Arr::get($this->services, $name);
     }
 }
--- src/json-rpc/src/ServiceCollector.php
+++ src/json-rpc/src/ServiceCollector.php
@@ -11,36 +11,23 @@ declare(strict_types=1);
 
 namespace Max\JsonRpc;
 
-use InvalidArgumentException;
 use Max\Aop\Collector\AbstractCollector;
-use Max\Di\Reflection;
 use Max\JsonRpc\Attribute\RpcService;
-use Max\Utils\Arr;
+use Psr\Container\ContainerExceptionInterface;
 use ReflectionException;
-use ReflectionMethod;
 
 class ServiceCollector extends AbstractCollector
 {
-    protected static array $container = [];
-
     /**
+     * @throws ContainerExceptionInterface
      * @throws ReflectionException
      */
     public static function collectClass(string $class, object $attribute): void
     {
         if ($attribute instanceof RpcService) {
             $service = $attribute->name;
-            if (isset(static::$container[$service])) {
-                throw new InvalidArgumentException('Service \'' . $service . '\' has been registered');
-            }
-            foreach (Reflection::methods($class, ReflectionMethod::IS_PUBLIC) as $reflectionMethod) {
-                static::$container[$service][$reflectionMethod->getName()] = [$class, $reflectionMethod->getName()];
-            }
+            dump($service);
+            make(Server::class)->register($service, $class);
         }
-    }
-
-    public static function getService(string $name)
-    {
-        return Arr::get(static::$container, $name);
     }
 }
