6b288d9918a98a092e78cf8d786c2e618cace1bd
ChengYao <bigyao@139.com>
2022-12-16 13:54:16 +0800

    Add: max/swoole

---
+++ src/swoole/tests/Concerns/UserTable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Max\Swoole\Tests\Concerns;
+
+use Max\Swoole\Table\Model;
+
+class UserTable extends Model
+{
+    protected static string $table    = 'users';
+    protected static array  $fillable = ['id', 'info'];
+    protected static array  $casts    = ['info' => 'json'];
+}
