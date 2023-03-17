fe3f53464d0129485cfe06e118a2ef0b4a7151b6
chengyao <bigyao@139.com>
2023-03-17 10:22:43 +0800

    Remove: 移除ConfigProvider

--- src/framework/src/Console/Kernel.php
+++ src/framework/src/Console/Kernel.php
@@ -2,17 +2,26 @@
 
 namespace Max\Console;
 
+use Max\Console\Command\ControllerMakeCommand;
+use Max\Console\Command\MiddlewareMakeCommand;
+use Max\Console\Command\RouteListCommand;
 use Symfony\Component\Console\Application;
 
 class Kernel extends Application
 {
+    private array $commands = [
+        RouteListCommand::class,
+        ControllerMakeCommand::class,
+        MiddlewareMakeCommand::class,
+    ];
+
     public function __construct(
         string $name = 'UNKNOWN',
         string $version = 'UNKNOWN'
     )
     {
         parent::__construct($name, $version);
-        foreach ($this->commands() as $command) {
+        foreach (array_merge($this->commands, $this->commands()) as $command) {
             $this->add(make($command));
         }
     }
