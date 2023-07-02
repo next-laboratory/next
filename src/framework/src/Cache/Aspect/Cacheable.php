fd8bcd2584885db21ed35041c66399d3a69122c0
chengyao <987861463@qq.com>
2023-07-02 10:34:33 +0800

    Perf: 优化Cacheable

--- src/framework/src/Cache/Aspect/Cacheable.php
+++ src/framework/src/Cache/Aspect/Cacheable.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * This file is part of MaxPHP.
+ * This file is part of MarxPHP.
  *
  * @link     https://github.com/marxphp
  * @license  https://github.com/marxphp/max/blob/master/LICENSE
@@ -11,37 +11,34 @@ declare(strict_types=1);
 
 namespace Max\Cache\Aspect;
 
-use Attribute;
-use Closure;
 use Max\Aop\Contract\AspectInterface;
 use Max\Aop\JoinPoint;
-use Max\Cache\CacheManager;
 use Psr\Container\ContainerExceptionInterface;
+use Psr\SimpleCache\CacheInterface;
 use Psr\SimpleCache\InvalidArgumentException;
-use ReflectionException;
 
-#[Attribute(Attribute::TARGET_METHOD)]
+#[\Attribute(\Attribute::TARGET_METHOD)]
 class Cacheable implements AspectInterface
 {
     public function __construct(
-        protected ?int $ttl = null,
         protected string $prefix = '',
-        protected ?string $key = null
+        protected ?string $key = null,
+        protected ?int $ttl = null,
     ) {
     }
 
     /**
      * @throws ContainerExceptionInterface
-     * @throws InvalidArgumentException|ReflectionException
+     * @throws InvalidArgumentException|\ReflectionException
      */
-    public function process(JoinPoint $joinPoint, Closure $next): mixed
+    public function process(JoinPoint $joinPoint, \Closure $next): mixed
     {
-        return make(CacheManager::class)->store()->remember($this->getKey($joinPoint), fn() => $next($joinPoint), $this->ttl);
+        return make(CacheInterface::class)->remember($this->getKey($joinPoint), fn () => $next($joinPoint), $this->ttl);
     }
 
     protected function getKey(JoinPoint $joinPoint): string
     {
-        $key = $this->key ?? ($joinPoint->class . ':' . $joinPoint->method . ':' . serialize(array_filter($joinPoint->parameters->getArrayCopy(), fn($item) => !is_object($item))));
+        $key = $this->key ?? ($joinPoint->class . ':' . $joinPoint->method . ':' . serialize(array_filter($joinPoint->parameters->getArrayCopy(), fn ($item) => ! is_object($item))));
         return $this->prefix ? ($this->prefix . ':' . $key) : $key;
     }
 }
