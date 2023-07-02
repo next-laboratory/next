44736849126a4383a129a31b1bc4b910662622e8
chengyao <987861463@qq.com>
2023-07-02 13:56:19 +0800

    Perf: 优化异常处理

--- src/http-server/src/Middleware/ExceptionHandleMiddleware.php
+++ src/http-server/src/Middleware/ExceptionHandleMiddleware.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * This file is part of MaxPHP.
+ * This file is part of MarxPHP.
  *
  * @link     https://github.com/marxphp
  * @license  https://github.com/marxphp/max/blob/master/LICENSE
@@ -19,26 +19,30 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Throwable;
 
 class ExceptionHandleMiddleware implements MiddlewareInterface
 {
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<Throwable>>
+     * @var array<int, class-string<\Throwable>>
      */
     protected array $dontReport = [];
 
     /**
-     * @throws Throwable
+     * @throws \Throwable
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         try {
-            return $handler->handle($request);
-        } catch (Throwable $e) {
-            if (!$this->shouldntReport($e)) {
+            set_error_handler(function ($errno, $errMsg, $errFile, $errLine) {
+                throw new \ErrorException($errMsg, 0, $errno, $errFile, $errLine);
+            });
+            $response = $handler->handle($request);
+            restore_error_handler();
+            return $response;
+        } catch (\Throwable $e) {
+            if (! $this->shouldntReport($e)) {
                 $this->report($e, $request);
             }
             return $this->render($e, $request);
@@ -48,20 +52,19 @@ class ExceptionHandleMiddleware implements MiddlewareInterface
     /**
      * 报告异常.
      */
-    protected function report(Throwable $e, ServerRequestInterface $request): void
+    protected function report(\Throwable $e, ServerRequestInterface $request): void
     {
     }
 
     /**
      * 将异常转为ResponseInterface对象
      */
-    protected function render(Throwable $e, ServerRequestInterface $request): ResponseInterface
+    protected function render(\Throwable $e, ServerRequestInterface $request): ResponseInterface
     {
         $message    = $e->getMessage();
         $statusCode = $this->getStatusCode($e);
         if (str_contains($request->getHeaderLine('Accept'), 'json')
             || strcasecmp('XMLHttpRequest', $request->getHeaderLine('X-REQUESTED-WITH')) === 0) {
-
             $body = json_encode([
                 'status'  => false,
                 'code'    => $statusCode,
@@ -71,32 +74,41 @@ class ExceptionHandleMiddleware implements MiddlewareInterface
 
             return new Response($statusCode, ['Content-Type' => 'application/json; charset=utf-8'], $body);
         }
+        $html = <<<'HTML'
+<html lang="zh">
+    <head>
+        <title>%s</title>
+        </head>
+        <body>
+<pre style="white-space: break-spaces">
+<b style="color: red">%s %s in %s:%d</b>
+<b>Stack Trace</b>
+%s
+</pre>
+        </body>
+</html>
+HTML;
 
-        $body = sprintf(
-            '<html lang="zh"><head><title>%s</title></head><body><pre style="white-space: break-spaces"><p><b style="color: red">%s</b></p><b>Stack Trace</b><br>%s</pre></body></html>',
-            $message,
-            $message,
-            $e->getTraceAsString(),
-        );
+        $body = sprintf($html, $message, $e::class, $message, $e->getFile(), $e->getLine(), $e->getTraceAsString());
 
         return new Response($statusCode, ['Content-Type' => 'text/html; charset=utf-8'], $body);
     }
 
-    protected function getStatusCode(Throwable $e)
+    protected function getStatusCode(\Throwable $e)
     {
         return $e instanceof HttpException ? $e->getCode() : StatusCodeInterface::STATUS_INTERNAL_SERVER_ERROR;
     }
 
     /**
-     * 忽略报告的异常
+     * 忽略报告的异常.
      */
-    protected function shouldntReport(Throwable $e): bool
+    protected function shouldntReport(\Throwable $e): bool
     {
-        return !is_null(Arr::first($this->dontReport, fn($type) => $e instanceof $type));
+        return ! is_null(Arr::first($this->dontReport, fn ($type) => $e instanceof $type));
     }
 
     /**
-     * 运行环境是否是cli
+     * 运行环境是否是cli.
      */
     protected function runningInConsole(): bool
     {
