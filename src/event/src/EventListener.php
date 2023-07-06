bd70a13d20cd2c02d98abadc641b38e783c4bc78
chengyao <bigyao@139.com>
2023-07-06 10:52:57 +0800

    Tests: 完善单元测试

--- src/event/src/EventListener.php
+++ src/event/src/EventListener.php
@@ -1,5 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * This file is part of MarxPHP.
+ *
+ * @link     https://github.com/marxphp
+ * @license  https://github.com/marxphp/max/blob/master/LICENSE
+ */
+
 namespace Max\Event;
 
 use Max\Event\Contract\EventListenerInterface;
--- src/event/tests/Events/FooEvent.php
+++ src/event/tests/Events/FooEvent.php
@@ -1,12 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * This file is part of MarxPHP.
+ *
+ * @link     https://github.com/marxphp
+ * @license  https://github.com/marxphp/max/blob/master/LICENSE
+ */
+
 namespace Max\Event\Tests\Events;
 
 class FooEvent
 {
     public function __construct(
         public string $value = '',
-    )
-    {
+    ) {
     }
 }
